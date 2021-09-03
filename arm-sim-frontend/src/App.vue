ca432a1532f99989e2243e7cc71f28f148be4bb1
Gabriel Carvalho <carvalho_biel@outlook.com>
2021-09-03 02:46:41 -0300

    Adiciona e ajustado tabelas

--- arm-sim-frontend/src/App.vue
+++ arm-sim-frontend/src/App.vue
@@ -1,68 +1,346 @@
 <template>
-    <div>
-        <div class="editor-area">
-            <sim-menu/>
-            <editor ref="tabbedEditor"/>
-        </div>
-        <div class="simulation-area">
-            <rom-table></rom-table>
-
-            <on-off-table title="Register Bank">
-            </on-off-table>
-
-            <on-off-table title="Data Memmory">
-            </on-off-table>
-
-            <on-off-table title="Device Memmory">
-            </on-off-table>
-        </div>
+  <div>
+    <div class="editor-area">
+      <sim-menu />
+      <editor ref="tabbedEditor" />
     </div>
+    <div class="simulation-area">
+      <rom-table></rom-table>
+    </div>
+
+    <div>
+      <div>
+        <div class="int-table">
+          <h2 class="int-h2">Menu de Interrupções</h2>
+          <div class="int-buttons">
+            <div class="first-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="second-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="third-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+          </div>
+          <div class="int-buttons">
+            <div class="first-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="second-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="third-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+          </div>
+          <div class="int-buttons">
+            <div class="first-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="second-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="third-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+          </div>
+          <div class="int-buttons">
+            <div class="first-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="second-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="third-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+          </div>
+          <div class="int-buttons">
+            <div class="first-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="second-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+            <div class="third-btn">
+              <button class="int-table-btn">INT 1</button>
+              <button class="int-table-input"></button>
+            </div>
+          </div>
+        </div>
+        <div class="arm-on-off-table">
+          <button
+            class="arm-table-btn"
+            v-on:click="
+              (hideRegister = !hideRegister), (ajustRegister = !ajustRegister)
+            "
+            v-bind:style="ajustRegister ? 'width: 100%' : 'width: 20%'"
+          >
+            Banco de Registradores
+          </button>
+          <table class="arm-table" v-if="!hideRegister">
+            <tr class="table-column">
+              <th class="table-item">AAXAA</th>
+              <th class="table-item">BBXBB</th>
+              <th class="table-item">CCXCC</th>
+              <th class="table-item">DDXDD</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+          </table>
+        </div>
+
+        <div class="arm-on-off-table">
+          <button
+            class="arm-table-btn"
+            v-on:click="(hideRam = !hideRam), (ajustRam = !ajustRam)"
+            v-bind:style="ajustRam ? 'width: 100%' : 'width: 20%'"
+          >
+            Memória RAM
+          </button>
+          <table class="arm-table" v-if="!hideRam">
+            <tr class="table-column">
+              <th class="table-item">AAXAA</th>
+              <th class="table-item">BBXBB</th>
+              <th class="table-item">CCXCC</th>
+              <th class="table-item">DDXDD</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+          </table>
+        </div>
+
+        <div class="arm-on-off-table">
+          <button
+            class="arm-table-btn"
+            v-on:click="(hideDisp = !hideDisp), (ajustDisp = !ajustDisp)"
+            v-bind:style="ajustDisp ? 'width: 100%' : 'width: 20%'"
+          >
+            Memória de Dispositivos
+          </button>
+          <table class="arm-table" v-if="!hideDisp">
+            <tr class="table-column">
+              <th class="table-item">AAXAA</th>
+              <th class="table-item">BBXBB</th>
+              <th class="table-item">CCXCC</th>
+              <th class="table-item">DDXDD</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+            <tr class="table-column">
+              <th class="table-item">NAME</th>
+              <th class="table-item">ADDRESS</th>
+              <th class="table-item">BINARY INSTRUCTION</th>
+              <th class="table-item">ASSEMBLY INSTRUCTION</th>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TabbedEditor from "./components/shared/custom-codemirror/tabbed-editor.vue";
+import ButtonsMenu from "./components/shared/buttons-menu/buttons-menu.vue";
+import RomTable from "./components/shared/rom-table/rom-table.vue";
 
-    import TabbedEditor from './components/shared/custom-codemirror/tabbed-editor.vue'
-    import ButtonsMenu from './components/shared/buttons-menu/buttons-menu.vue'
-    import OnOffTable from './components/shared/on-off-table/on-off-table.vue'
-    import RomTable from './components/shared/rom-table/rom-table.vue'
+export default {
+  name: "App",
+  components: {
+    editor: TabbedEditor,
+    "sim-menu": ButtonsMenu,
+    "rom-table": RomTable,
+  },
 
-    export default {
+  data() {
+    return {
+      hideDisp: false,
+      ajustRegister: true,
+      ajustRam: true,
+      ajustDisp: true,
+      hideRam: false,
+      hideRegister: false,
+      perPage: 3,
+      currentPage: 1,
+      items: [
+        { id: 1, first_name: "Fred", last_name: "Flintstone" },
+        { id: 2, first_name: "Wilma", last_name: "Flintstone" },
+        { id: 3, first_name: "Barney", last_name: "Rubble" },
+        { id: 4, first_name: "Betty", last_name: "Rubble" },
+        { id: 5, first_name: "Pebbles", last_name: "Flintstone" },
+        { id: 6, first_name: "Bamm Bamm", last_name: "Rubble" },
+        { id: 7, first_name: "The Great", last_name: "Gazzoo" },
+        { id: 8, first_name: "Rockhead", last_name: "Slate" },
+        { id: 9, first_name: "Pearl", last_name: "Slaghoople" },
+      ],
+    };
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    },
+  },
 
-        name: 'App',
-        components: {
-            "editor": TabbedEditor,
-            "sim-menu": ButtonsMenu,
-            "on-off-table": OnOffTable,
-            "rom-table": RomTable
-        },
-
-        data() {
-            return {
-                data_memmory_fields: [
-                    {
-                        name: "teste",
-                        value: "BBxBB"
-                    },
-                    {
-                        name: "teste",
-                        value: "BBxBB"
-                    },
-                    {
-                        name: "teste",
-                        value: "BBxBB"
-                    },
-                    {
-                        name: "teste",
-                        value: "BBxBB"
-                    }
-                ]
-            }
-        },
-
-        methods: {
-
-        }
-    }
-
+  methods: {},
+};
 </script>
+<style >
+.arm-table-btn {
+  background-color: #f5e68f;
+  border: none;
+  color: #756e44;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  width: 100%;
+}
+
+.arm-on-off-table {
+  padding: 2% 2% 2% 2%;
+  display: flex;
+  flex-direction: column;
+}
+
+.table-column {
+  display: flex;
+  flex-direction: row;
+}
+
+.table-item {
+  justify-content: space-around;
+  margin: 1px;
+  width: 70%;
+  color: #756e44;
+  border: 1px solid #f5e68f;
+  box-shadow: 1px 2px #888888;
+}
+
+.arm-table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.int-table {
+  margin-top: 2%;
+  width: 50%;
+  margin-left: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  border: 1px solid;
+}
+
+.int-buttons {
+  display: flex;
+  justify-content: space-around;
+  margin: 2px;
+}
+
+.int-table-btn {
+  background-color: #f5e68f; /* Green */
+  border: none;
+  margin-bottom: 2px;
+  color: #888888;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.int-table-input {
+  background-color: white; /* Green */
+  border: 1px solid #aa9106;
+  margin-bottom: 2px;
+  color: #888888;
+  padding: 20px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.first-btn,
+.second-btn,
+.third-btn {
+  padding: 4px;
+}
+
+.int-h2 {
+  margin-top: 2%;
+  border: 0.8px solid;
+  padding: 5px;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  width: 98.7%;
+  text-align: center;
+}
 </style>
--- arm-sim-frontend/src/components/shared/rom-table/rom-table.vue
+++ arm-sim-frontend/src/components/shared/rom-table/rom-table.vue
@@ -2,48 +2,48 @@
     <div class="arm-rom">
         <h2 class="rom-h2">ROM</h2>
 
-        <table class="rom-table">
-            <tr class="table-column">
-                <th class="table-item">NAME</th>
-                <th class="table-item">ADDRESS</th>
-                <th class="table-item">BINARY INSTRUCTION</th>
-                <th class="table-item">ASSEMBLY INSTRUCTION</th>
+        <table class="rom-table" cellspacing="0" cellpadding="0">
+            <tr class="rom-table-column">
+                <th class="rom-table-item">NAME</th>
+                <th class="rom-table-item">ADDRESS</th>
+                <th class="rom-table-item">BINARY INSTRUCTION</th>
+                <th class="rom-table-item">ASSEMBLY INSTRUCTION</th>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Alfreds Futterkiste</td>
-                <td class="table-item">Maria Anders</td>
-                <td class="table-item">Germany</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Alfreds Futterkiste</td>
+                <td class="rom-table-item">Maria Anders</td>
+                <td class="rom-table-item">Germany</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Centro comercial Moctezuma</td>
-                <td class="table-item">Francisco Chang</td>
-                <td class="table-item">Mexico</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Centro comercial Moctezuma</td>
+                <td class="rom-table-item">Francisco Chang</td>
+                <td class="rom-table-item">Mexico</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Ernst Handel</td>
-                <td class="table-item">Roland Mendel</td>
-                <td class="table-item">Austria</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Ernst Handel</td>
+                <td class="rom-table-item">Roland Mendel</td>
+                <td class="rom-table-item">Austria</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Island Trading</td>
-                <td class="table-item">Helen Bennett</td>
-                <td class="table-item">UK</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Island Trading</td>
+                <td class="rom-table-item">Helen Bennett</td>
+                <td class="rom-table-item">UK</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Laughing Bacchus Winecellars</td>
-                <td class="table-item">Yoshi Tannamuri</td>
-                <td class="table-item">Canada</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Laughing Bacchus Winecellars</td>
+                <td class="rom-table-item">Yoshi Tannamuri</td>
+                <td class="rom-table-item">Canada</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
-            <tr class="table-column">
-                <td class="table-item">Magazzini Alimentari Riuniti</td>
-                <td class="table-item">Giovanni Rovelli</td>
-                <td class="table-item">Italy</td>
-                <td class="table-item">Alfreds Futterkiste</td>
+            <tr class="rom-table-column">
+                <td class="rom-table-item">Magazzini Alimentari Riuniti</td>
+                <td class="rom-table-item">Giovanni Rovelli</td>
+                <td class="rom-table-item">Italy</td>
+                <td class="rom-table-item">Alfreds Futterkiste</td>
             </tr>
         </table>
     </div>
@@ -91,17 +91,26 @@
 
     .rom-table {
         font-family: arial, sans-serif;
-        border-collapse: collapse;
         width: 100%;
+        border: 1px solid;
     }
-
-    .table-column {
-
-    }
-
-    .table-item {
-        border: 1px solid #756E44;
+    
+    .rom-table-item {
         padding: 8px;
+        
+    }
+
+    tr, td, th {
+        margin: 0;
+    }
+
+    th {
+        text-align: left;
         font-weight: bold;
+        border-bottom: 1px solid;
+    }
+
+    td {
+        border-bottom: 1px solid gray;
     }
 </style>
